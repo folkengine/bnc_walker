445038b73b02960f15cd1caf3e501447eee19502
Christoph <ignatz@gmail.com>
2017-10-15 12:43:02 -0400

    copied over working storyboard and ViewController

--- BnCWater/ViewController.swift
+++ BnCWater/ViewController.swift
@@ -1,36 +1,36 @@
 //
 //  ViewController.swift
-//  BnCWater
+//  Test
 //
-//  Created by Christopher Baker on 6/27/16.
-//  Copyright © 2016 Christopher Baker. All rights reserved.
+//  Created by Christoph on 10/15/17.
+//  Copyright © 2017 Christopher Baker. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var matrixTextField: UITextView!
-    @IBOutlet var successfulTextField: UITextField!
-    @IBOutlet var sumTextField: UITextField!
-    @IBOutlet var pathTextField: UITextField!
-    @IBOutlet var actionButton: UIButton!
-    
-    @IBOutlet var resistanceSlider: UISlider!
-    @IBOutlet var resistanceLevel: UITextField!
 
+    @IBOutlet weak var pathTextField: UITextField!
+    @IBOutlet weak var sumTextField: UITextField!
+    @IBOutlet weak var successfulTextField: UITextField!
+    @IBOutlet weak var matrixTextView: UITextView!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var resistanceSlider: UISlider!
+    @IBOutlet weak var resistanceLevel: UITextField!
+    
     var resistance: Int = 50
     var matrix: BoundlessMatrix!
-    
-    @IBAction func sliderValueChanged(sender: UISlider) {
-        resistance = Int(sender.value)
-        resistanceLevel.text = "\(resistance)"
+
+    @IBAction func begin(_ sender: UIButton) {
+        let cart = Cartesian(x:8, y: 8)
+        matrix = BoundlessMatrix.factory(dimensions: cart)
+        NSLog("begin()")
         processMatrix()
     }
     
-    @IBAction func begin(sender : AnyObject) {
-        let cart = Cartesian(x:8, y: 5)
-        matrix = BoundlessMatrix.factory(dimensions: cart)
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        resistance = Int(sender.value)
+        resistanceLevel.text = "\(resistance)"
         processMatrix()
     }
     
@@ -40,37 +40,23 @@ class ViewController: UIViewController {
         let result = Result(threshold: resistance, path: walker.bestWalk)
         
         NSLog("\(result)")
+        matrixTextView.text = matrix.description
+        successfulTextField.text = result.successful ? "Great Success!" : "Unacceptable!!!"
+        sumTextField.text = "SUM: \(String(result.sum))"
         
-        matrixTextField.text = matrix.description
-        successfulTextField.text = result.successful ? "Yes" : "No"
-        sumTextField.text = String(result.sum)
         let walk: String = Walker.calculateWalk(trail: result.completed).map({String($0)}).joined(separator: "\t")
         
         pathTextField.text = "\(walk)"
-        resistanceLevel.text = "\(resistance)"
-        
-        matrixTextField.textAlignment = NSTextAlignment.center
     }
 
     override func viewDidLoad() {
-        styleActionButton()
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    func styleActionButton() {
-        actionButton.backgroundColor = UIColor.darkGray
-        actionButton.layer.cornerRadius = 5
-        actionButton.layer.borderWidth = 1
-        actionButton.layer.borderColor = UIColor.black.cgColor
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func centerText() {
-        var topCorrect : CGFloat = (matrixTextField.frame.height - matrixTextField.contentSize.height);
-        topCorrect = topCorrect < 0.0 ? 0.0 : topCorrect / 2
-        matrixTextField.contentOffset = CGPoint(x: 0, y: -topCorrect)
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
+    
 }
